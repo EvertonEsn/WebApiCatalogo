9962a218d71f8507fbedf089ce8045d40172ba71
evertonnascimentogc <everton.nascimento@guardachuvatecnologia.com.br>
2025-08-05 19:08:42 -0300

    Aplicando Data Annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace APICatalogo.Models;
 // So tem atributos -> Classe anemica
+
+[Table("Categorias")]
 public class Categoria
 {
+    [Key]
     public int CategoriaId { get; set; }
-
+    
+    [Required]
+    [StringLength(80)]
     public string? Nome  { get; set; }
     
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
     
     public ICollection<Produto>? Produtos { get; set; }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,17 +1,30 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 // So tem atributos -> Classe anemica
+
+[Table("Produtos")]
 public class Produto
 {
+    [Key]
     public int ProdutoId { get; set; }
     
+    [Required]
+    [StringLength(80)]
     public string? Nome  { get; set; }
     
+    [Required]
+    [StringLength(300)]
     public string? Descricao { get; set; }
     
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
     public decimal Preco { get; set; }
     
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
     
     public float Estoque { get; set; }
